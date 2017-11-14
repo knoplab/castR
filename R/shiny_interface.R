0973d5d3f5c7b54de1948b110510063460372089
Benjamin C B <33428350+ninjaminb@users.noreply.github.com>
2017-11-14 10:22:23 +0100

    Update shiny_interface.R

--- R/shiny_interface.R
+++ R/shiny_interface.R
@@ -1,1 +1,20 @@
-s
+#' Start an Interactive castR Session
+#'
+#' Starts a Shiny application, in which the castR functionality can be accessed
+#' interactively. The data is stored in the user's workspace and can be written
+#' to a local device.
+#' @param ... Parameters to be passed to \code{shiny::runApp()}.
+#' @return Void.
+#' @import shiny
+#' @import data.table
+#' @export
+
+run_castR <- function(...) {
+  
+  options(shiny.maxRequestSize = 50 * 1024 ^ 2)
+  
+  shiny::runApp(shiny::shinyApp(
+    ui = CASTLING_ui, server = CASTLING_server
+  ), ...)
+  
+}
